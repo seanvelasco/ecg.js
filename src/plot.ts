e4eb37321b6f67d7d9c92d10a635d91eacf46702
Sean Velasco <seanvelasco@ieee.org>
2023-01-10 13:37:37 +0800

    Replaced plot functions

--- src/plot.ts
+++ src/plot.ts
@@ -1,15 +1,14 @@
-import jsdom from 'jsdom'
+import { JSDOM } from 'jsdom'
 import * as d3 from 'd3'
 
 const plot = (data: [number, number][][], labels?: string[], barcode?: string) => {
 
 	const lineColor = '#000000'
-	const lineWidth = 0.75 // 0.5
+	const lineWidth = 0.75
 	const minorGridColor = '#ffbfc0'
 	const majorGridColor = '#ff7f7f'
 	const minorGridWidth = 0.5
 	const majorGridWidth = 0.5
-	const borderWidth = 2
 	const textColor = '#000000'
 	const textSize = '0.75em'
 	const textFont = 'sans-serif'
@@ -17,6 +16,28 @@ const plot = (data: [number, number][][], labels?: string[], barcode?: string) =
 	const DPI = 120
 	const height = (30 * DPI) / 25.4
 	const width = (250 * DPI) / 25.4
+
+	// DOM creation
+
+	const { document } = new JSDOM('').window
+
+	const svg = d3.select(document).select('body').append('svg')
+
+	svg.append('title').text(barcode || 'ECG Chart')
+
+	svg
+		.attr('xmlns', 'http://www.w3.org/2000/svg')
+		.attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+		.attr('width', width)
+		.attr('height', height * 4)
+		.attr('shape-rendering', "crispEdges")
+		.style('fill', 'none') // Makes the line not filled
+		.style('stroke-width', 0)
+
+	svg.append('rect')
+		.attr('width', '100%')
+		.attr('height', '100%')
+		.attr('fill', 'white')
 
 	const xScale = d3.scaleLinear().range([0, width]).domain([0, 5000])
 
@@ -26,101 +47,50 @@ const plot = (data: [number, number][][], labels?: string[], barcode?: string) =
 
 	const yScale = d3.scaleLinear().range([height, 0]).domain([-300, 300])
 
-	// DOM creation
+	const line = d3.line()
+		.x((d: [number, number]) => xScale(d[0]))
+		.y((d: [number, number]) => yScale(d[1]))
 
-	const { JSDOM } = jsdom
+	const xMajorGrid = d3.axisTop(xScale).ticks(50).tickSize(-height).offset(0).tickFormat(() => '')
+	const xMinorGrid = d3.axisTop(xScale).ticks(50 * 5).tickSize(-height).offset(0).tickFormat(() => '')
 
-	const { document } = new JSDOM('').window
+	const yMajorGrid = d3.axisLeft(yScale).ticks(6).tickSize(-width).offset(0).tickFormat(() => '')
+	const yMinorGrid = d3.axisLeft(yScale).ticks(6 * 5).tickSize(-width).offset(0).tickFormat(() => '')
 
-	const body = d3.select(document).select('body')
+	// Generate grid
 
-	const svg = body
-		.append('svg')
-		.attr('xmlns', 'http://www.w3.org/2000/svg')
-		.attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
-		.attr('width', width)
-		.attr('height', height)
-		.attr('style', `border: ${borderWidth / 2}px solid ${majorGridColor};`) // Outer border
-		.attr('shape-rendering', "crispEdges")
-
-	// add rect
-	svg.append('rect')
-		.attr('width', width)
-		.attr('height', height)
-		.attr('fill', 'white')
-
-	const generateMetadata = () => {
-		if (barcode) {
-			svg.append('title').text(barcode)
-		} else {
-			svg.append('title').text('ECG Chart')
-		}
-	}
-
-	const generateGrid = () => {
-		const xMajorAxisGrid = d3.axisTop(xScale).tickValues(xMajorTicks).tickSize(-height)
-
-		const yMajorAxisGrid = d3.axisLeft(yScale).tickValues(yMajorTicks).tickSize(-width)
-
-		const xMinorAxisGrid = d3.axisTop(xScale).tickValues(xMinorTicks).tickSize(-height)
-
-		const yMinorAxisGrid = d3.axisLeft(yScale).tickValues(yMinorTicks).tickSize(-width)
-
-		// Render minor grids first
-		// Otherwise, minor grids will cover major grids
+	for (const channel in d3.range(data.length)) {
 
 		svg
 			.append('g')
-			.attr('class', 'minor-grid')
 			.style('color', minorGridColor)
 			.style('stroke-width', minorGridWidth)
-			.call(xMinorAxisGrid)
+			.attr('transform', `translate(0, ${height * parseInt(channel)})`)
+			.call(xMinorGrid)
 
 		svg
 			.append('g')
-			.attr('class', 'minor-grid')
 			.style('color', minorGridColor)
 			.style('stroke-width', minorGridWidth)
-			.call(yMinorAxisGrid)
+			.attr('transform', `translate(0, ${height * parseInt(channel)})`)
+			.call(yMinorGrid)
 
 		svg
 			.append('g')
-			.attr('class', 'major-grid')
 			.style('color', majorGridColor)
 			.style('stroke-width', majorGridWidth)
-			.call(xMajorAxisGrid)
+			.attr('transform', `translate(0, ${height * parseInt(channel)})`)
+			.call(xMajorGrid)
 
 		svg
 			.append('g')
-			.attr('class', 'major-grid')
 			.style('color', majorGridColor)
 			.style('stroke-width', majorGridWidth)
-			.call(yMajorAxisGrid)
-
-		// Offset grids by 0.5 to balance off extra width
-		// Both x, y major & minor grids contribute 0.5 extra width
-		// Move grid upwards and towards the left to hide extra ticks
-
-		svg.selectAll('.major-grid ').attr('transform', 'translate(-0.5, -0.5)')
-
-		svg.selectAll('.minor-grid ').attr('transform', 'translate(-0.5, -0.5)')
+			.attr('transform', `translate(0, ${height * parseInt(channel)})`)
+			.call(yMajorGrid)
 	}
 
-	const generateBorder = () => {
-		svg
-			.append('rect')
-			.attr('x', 0)
-			.attr('y', 0)
-			.attr('width', width)
-			.attr('height', height)
-			.style('fill', 'none')
-			.style('stroke', majorGridColor)
-			.style('stroke-width', borderWidth / 2) // Inner border
-	}
-
-	const generateLabels = (text: string, x: number, y: number) => {
-		// Center: (width / 2) - (textWidth / 2) & (height / 2) - (textHeight / 2)
-
+	const generateLabel = (text: string, x: number, y: number) => {
 		svg
 			.append('text')
 			.attr('x', x)
@@ -131,88 +101,34 @@ const plot = (data: [number, number][][], labels?: string[], barcode?: string) =
 			.attr('font-family', textFont)
 			.attr('fill', textColor)
 			.text(text)
+
 	}
 
-	const group1 = ['I', 'II', 'III', 'II']
-	const group2 = ['aVR', 'aVL', 'aVF']
-	const group3 = ['V1', 'V2', 'V3']
-	const group4 = ['V4', 'V5', 'V6']
+	// Generate lines and labels
 
-	const generateLines = () => {
-		for (const lead of range(rows)) {
-			const displacement = lead * (3 * cm) * pixels
+	for (const channel in d3.range(data.length)) {
+		svg.append('path')
+			.attr('d', line(data[channel]))
+			.attr('transform', `translate(0, ${height * parseInt(channel)})`)
+			.style('stroke', lineColor)
+			.style('stroke-width', lineWidth)
+			.attr('shape-rendering', "geometricPrecision")
+			.attr('stroke-linejoin', 'round')
 
-			// this dispalces the chart
-			for (const point in data[lead]) {
-				data[lead][point][1] -= displacement
-			}
+		const group1 = ['I', 'aVR', 'V1', 'V4']
+		const group2 = ['II', 'aVL', 'V2', 'V5']
+		const group3 = ['III', 'aVF', 'V3', 'V6']
+		const group4 = ['II']
 
-			const line = d3
-				.line()
-				.x((d: [number, number]) => xScale(d[0]))
-				.y((d: [number, number]) => yScale(d[1]))
+		generateLabel(group1[channel], (width / 4 * parseInt(channel)), (height / 2 - 25))
+		generateLabel(group2[channel], ((width) / 4 * parseInt(channel)), (height / 2 + height - 25))
+		generateLabel(group3[channel], ((width) / 4 * parseInt(channel)), (height / 2 + (height * 2) - 25))
+		generateLabel(group4[channel], ((width) / 4 * parseInt(channel)), (height / 2 + (height * 3) - 25))
 
-			svg
-				.append('path')
-				.attr('d', line(data[lead]))
-				.style('stroke', lineColor)
-				.style('stroke-width', lineWidth)
-				.style('fill', 'none')
-
-			// Hardcoded coordinates for texts
-			// Ideally, these would be calculated based on page dimensions
-
-
-			const offset = 10
-			const yOffset = 20
-
-			generateLabels(group1[lead], (width / 4) * 0 + offset, height / 8 + displacement - yOffset)
-			generateLabels(group2[lead], (width / 4) * 1 + offset, height / 8 + displacement - yOffset)
-			generateLabels(group3[lead], (width / 4) * 2 + offset, height / 8 + displacement - yOffset)
-			generateLabels(group4[lead], (width / 4) * 3 + offset, height / 8 + displacement - yOffset)
-		}
 	}
 
-	// const generateLines = () => {
-	// 	for (const lead of range(rows)) {
-	// 		const offset = lead * (3 * cm) * pixels
+	return document.documentElement.getElementsByTagName('svg')[0].outerHTML
 
-	// 		for (const point in data[lead]) {
-	// 			data[lead][point][1] -= offset
-	// 		}
-
-	// 		const line = d3
-	// 			.line()
-	// 			.x((d: any) => xScale(d[0]))
-	// 			.y((d: any) => yScale(d[1]))
-
-	// 		svg
-	// 			.append('path')
-	// 			.attr('d', line(data[lead]))
-	// 			.style('stroke', lineColor)
-	// 			.style('stroke-width', lineWidth)
-	// 			.style('fill', 'none')
-
-	// 		// Hardcoded coordinates for texts
-	// 		// Ideally, these would be calculated based on page dimensions
-	// 		generateLabels(leadLabels[lead], 22, 54 + offset)
-	// 	}
-	// }
-
-	// Take note that rendering the grid is independent from rendering the lines
-	// A change in the position of the lines will not affect the grid
-	// Likewise, a change in the position of the grid will not affect the lines
-	// However, the data & scale from which the lines and grid are drawn are the same
-
-	// Order of rendering is important
-	generateMetadata()
-	generateGrid()
-	generateLines()
-	generateBorder()
-
-	const svgElement = document.documentElement.getElementsByTagName('svg')[0].outerHTML
-
-	return svgElement
 }
 
 export {
